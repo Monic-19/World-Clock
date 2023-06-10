eb4c74df6d98333883b4ef74156d1448966d38cd
monic vyas <118365854+Monic-19@users.noreply.github.com>
2023-06-10 14:41:52 +0530

    hour and min hand done

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-const date = new Date();
-document.getElementById('usa').innerText= date.toLocaleString('en-US', {timeZone:'America/Los_angeles'} ).split(',')[1] ;
-document.getElementById('ind').innerText= date.toLocaleString('en-US', {timeZone:'Asia/Kolkata'} ).split(',')[1] ;
-document.getElementById('can').innerText= date.toLocaleString('en-US', {timeZone:'Canada/Atlantic'} ).split(',')[1] ;
-
 const updateTime = () =>{
   const date = new Date();
   document.getElementById('usa').innerText= date.toLocaleString('en-US', {timeZone:'America/Los_angeles'} ).split(',')[1] ;
@@ -11,3 +5,61 @@ const updateTime = () =>{
   document.getElementById('can').innerText= date.toLocaleString('en-US', {timeZone:'Canada/Atlantic'} ).split(',')[1] ; 
 }
 setInterval(updateTime,1000)
+
+
+
+function updateClocku(time){
+    console.log(time);
+    let htime=time.split(':')[0];
+    let mtime=time.split(':')[1];
+    let stime=time.split(':')[2];
+  
+    console.log(stime);
+  
+      hrotation = 30*htime + mtime/2;
+      mrotation = 6*mtime;
+      srotation = 6*stime;
+  
+      Hourhandu.style.transform = `rotate(${hrotation}deg)`;
+      Minhandu.style.transform = `rotate(${mrotation}deg)`;
+      Sechandu.style.transform = `rotate(${srotation}deg)`;
+  }
+  
+  function updateClocki(time){
+    console.log(time);
+    let htime=time.split(':')[0];
+    let mtime=time.split(':')[1];
+    let stime=time.split(':')[2];
+  
+    console.log(stime);
+  
+      hrotation = 30*htime + mtime/2;
+      mrotation = 6*mtime;
+      srotation = 6*stime;
+  
+      Hourhandi.style.transform = `rotate(${hrotation}deg)`;
+      Minhandi.style.transform = `rotate(${mrotation}deg)`;
+      Sechandi.style.transform = `rotate(${srotation}deg)`;
+  }
+  
+  function updateClockc(time){
+    console.log(time);
+    let htime=time.split(':')[0];
+    let mtime=time.split(':')[1];
+    let stime=time.split(':')[2];
+  
+    console.log(stime);
+  
+      hrotation = 30*htime + mtime/2;
+      mrotation = 6*mtime;
+      srotation = 6*stime;
+  
+      Hourhandc.style.transform = `rotate(${hrotation}deg)`;
+      Minhandc.style.transform = `rotate(${mrotation}deg)`;
+      Sechandc.style.transform = `rotate(${srotation}deg)`;
+  }
+  
+  
+  
+  
+  
