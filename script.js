9daf039fcbce96fd11922bf1318cba0da27d468c
monic vyas <118365854+Monic-19@users.noreply.github.com>
2023-06-11 15:29:19 +0530

    css and js done

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const updateTime = () =>{
   document.getElementById('usa').innerText= date.toLocaleString('en-US', {timeZone:'America/Los_angeles'} ).split(',')[1] ;
   document.getElementById('ind').innerText= date.toLocaleString('en-US', {timeZone:'Asia/Kolkata'} ).split(',')[1] ;
   document.getElementById('can').innerText= date.toLocaleString('en-US', {timeZone:'Canada/Atlantic'} ).split(',')[1] ; 
+
+  let t1=date.toLocaleString('en-US', {timeZone:'America/Los_angeles'} ).split(',')[1] ;
+  updateClocku(t1);
+
+  let t2=date.toLocaleString('en-US', {timeZone:'Asia/Kolkata'} ).split(',')[1] ;
+   updateClocki(t2);
+
+  let t3=date.toLocaleString('en-US', {timeZone:'Canada/Atlantic'} ).split(',')[1] ;
+   updateClockc(t3);
+
 }
 setInterval(updateTime,1000)
 
@@ -13,7 +23,8 @@ function updateClocku(time){
     let htime=time.split(':')[0];
     let mtime=time.split(':')[1];
     let stime=time.split(':')[2];
-  
+    stime=parseInt(stime);
+
     console.log(stime);
   
       hrotation = 30*htime + mtime/2;
@@ -30,6 +41,7 @@ function updateClocku(time){
     let htime=time.split(':')[0];
     let mtime=time.split(':')[1];
     let stime=time.split(':')[2];
+    stime=parseInt(stime);
   
     console.log(stime);
   
@@ -47,7 +59,8 @@ function updateClocku(time){
     let htime=time.split(':')[0];
     let mtime=time.split(':')[1];
     let stime=time.split(':')[2];
-  
+    stime=parseInt(stime);
+
     console.log(stime);
   
       hrotation = 30*htime + mtime/2;
@@ -61,5 +74,70 @@ function updateClocku(time){
   
   
   
+function uopenit(){
+let a = document.getElementById('u');
+let b = document.getElementById('ubtn');
+let c =document.getElementById('ucard');
+console.log(c);
+
+if(a.style.display != "none"){
+  a.style.display="none";
+  c.style.height="220px";
+  b.style.background="white";
+  b.style.color="black";
+}
+else{
+  a.style.display="block"
+  b.style.background="black";
+  b.style.color="white";
+  c.style.height="420px";
+}
+
+}
+
+
+function iopenit(){
+  let a=document.getElementById('i');
+  let b = document.getElementById('ibtn')
+  let c =document.getElementById('icard');
+  
+  if(a.style.display != "none"){
+    a.style.display="none";
+    c.style.height="220px";
+    b.style.background="white";
+    b.style.color="black";
+  }
+  else{
+    a.style.display="block"
+    b.style.background="black";
+    b.style.color="white";
+    c.style.height="420px";
+  }
+  
+  }
+  
+  function copenit(){
+    let a=document.getElementById('c');
+
+    let b = document.getElementById('cbtn');
+    let c =document.getElementById('ccard');
+
+    
+    if(a.style.display != "none"){
+      a.style.display="none";
+      c.style.height="220px";
+      b.style.background="white";
+      b.style.color="black";
+    }
+    else{
+      a.style.display="block"
+      b.style.background="black";
+      b.style.color="white";
+      c.style.height="420px";
+    }
+    
+    }
+    
+      
   
   
